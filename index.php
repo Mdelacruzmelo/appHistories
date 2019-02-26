93abedc820db7ad542c551435d818c347305ae41
Mdelacruzmelo <mdelacruzmelo@gmail.com>
2019-02-26 21:57:34 +0100

    Project WIP

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
   <body>
     <div id="container" class="container bg-danger position-relative">
 
-      <div id="loginContainer" class="col-lg-4 col-md-4 col-sm-8 p-4 bg-warning rounded1rem">
+      <div id="loginContainer" class="col-lg-3 col-md-4 col-sm-8 ml-lg-4 px-4 py-4 bg-warning rounded1rem">
         <form name="login" action="\login" method="POST">
           <div class="col-12 text-center">Login</div>
           <div class="form-group row mt-3">
@@ -30,8 +30,8 @@
                      aria-describedby="emailHelp" 
                      placeholder="Enter email">
               <small id="emailHelp" 
-                     class="form-text text-muted">
-                Please fill insert and email and password.
+                     class="form-text text-muted d-none">
+                Email does not exst.
               </small>
             </div>
 
@@ -39,30 +39,22 @@
           
           <div class="form-group row mt-3">
             <div class="col-1 mt-2 ml-2 p-0 pr-2">
-              <label for="email_login" class="widthHeight1rem rounded-circle bg-success float-right"></label>
+              <label for="password_login" class="widthHeight1rem rounded-circle bg-success float-right"></label>
             </div>
             <div class="col-10 p-0 px-2">
-              <input type="email" 
+              <input type="password" 
                      class="form-control"
-                     id="email_login" 
-                     aria-describedby="emailHelp" 
+                     id="password_login" 
+                     aria-describedby="passwordHelp" 
                      placeholder="Password">
-              <small id="emailHelp" 
-                     class="form-text text-muted">
-                Please fill insert and email and password.
+              <small id="passwordHelp" 
+                     class="form-text text-muted d-none">
+                Incorrect password
               </small>
             </div>
 
           </div>
           
-          
-          <div class="form-group row">
-            <label for="password_login">Password</label>
-            <input type="password" 
-                   class="form-control" 
-                   id="password_login" 
-                   placeholder="Password">
-          </div>
           <div class="form-check">
             <input type="checkbox" 
                    class="form-check-input" 
